0f000675ba84f1e26f4b378710c730efd36c2832
Kacha Mukabe <kachamukabe@gmail.com>
2017-07-27 10:27:01 +0200

    Simple login system implemented

--- date_me/urls.py
+++ date_me/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     #home
     url(r'^$', views.index, name='index'),
-    
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'index'} , name='logout'),
    
 #     url(r'^$',views.detail,name=''),
    
