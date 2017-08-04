7f704b1ca6af188b9daa18b501bd1f05bec276a7
Jessica <[mildrednwonye@gmail.com]>
2017-08-04 08:30:32 +0200

    Added Css

--- date_me/urls.py
+++ date_me/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "date_me"
+
 urlpatterns = [
     #home
     url(r'^$', views.index, name='index'),
     
    
-#     url(r'^$',views.detail,name=''),
+    #     url(r'^$',views.detail,name=''),
    
 ]
